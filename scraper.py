d7f916aec911bc90b76ee61b1abea5391a481d88
Arthur Abeilice <afa7789@gmail.com>
2025-06-13 17:48:02 -0300

    Update User-Agent string in scraper initialization for improved compatibility

--- scraper.py
+++ scraper.py
@@ -9,7 +9,7 @@ class MarketRoxoScraper:
         """Initializes the scraper with the base URL and headers."""
         self.base_url = base_url
         self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.7151.103  Safari/537.36"
         }
         self.delay = 25  # Seconds between requests
         # Assuming log_callback is already defined
