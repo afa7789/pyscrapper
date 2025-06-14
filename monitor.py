010638f66a7373a66dd8cfaf9e0cca54d7a9e1db
Arthur Abeilice <afa7789@gmail.com>
2025-06-14 17:14:41 -0300

    fix: Update scraper to use a batch size of 3 for improved ad retrieval

--- monitor.py
+++ monitor.py
@@ -112,7 +112,7 @@ class Monitor:
                 #  when we add multiple scrapers, we can loop through them
                 #  and scrape each adding the results to new_ads
                 new_ads = self.scraper.scrape(
-                    self.keywords, self.negative_keywords_list, 2)
+                    self.keywords, self.negative_keywords_list, 3)
 
                 # Filter out already seen ads using hashes
                 truly_new_ads = []
